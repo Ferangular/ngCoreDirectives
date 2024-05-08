451d9681ba610f3681580506c63c76f3e81de057
Firthuns <firthuns@gmail.com>
2024-05-08 12:14:37 +0200

    install jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngCoreDirectives';
-  console.log('hola');
+
   hola(){
     console.log('hola');
+
   }
 
 
