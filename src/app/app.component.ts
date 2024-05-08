5f78c601da8186fbfb7848237b6efe80d089e552
Firthuns <firthuns@gmail.com>
2024-05-08 12:45:33 +0200

    pre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ],
+
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngCoreDirectives';
 
-  hola(){
+  hola() {
     console.log('hola');
-
   }
-
-
 }
